21795c8a94c111264c1b1044afc114041e9a7ae9
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-10-08 23:11:43 +0530

    feat: learning day one

---
+++ learning-tailwind/basic-learning-one/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
+        <div className="bg-red-500 p-8 m-1 text-xl text-center">h1</div>
+        <div className="bg-yellow-500 p-8 m-1 text-xl text-center">h1</div>
+        <div className="bg-green-400 p-8 m-1 text-xl text-center">h1</div>
+        <div className="bg-blue-400 p-8 m-1 text-xl text-center">h1</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
